1cbe7b815144756b62d0e1f1c2352461d1d9ac0c
liujian(ROBERT) <liujian891010@gmail.com>
2026-05-08 10:04:56 +0800

    feat: add AG05 desktop sync runtime assembly

---
+++ clients/desktop/__init__.py
@@ -0,0 +1,13 @@
+from .sync_runtime import (
+    DesktopSyncHttpConfig,
+    DesktopSyncRuntime,
+    build_desktop_sync_runtime,
+    build_desktop_sync_session,
+)
+
+__all__ = [
+    "DesktopSyncHttpConfig",
+    "DesktopSyncRuntime",
+    "build_desktop_sync_runtime",
+    "build_desktop_sync_session",
+]
